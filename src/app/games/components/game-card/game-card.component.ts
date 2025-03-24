ba2595becb1e7fcbb1e30d9af6c2076cc1a5cf1a
Juan Velasco <juanvelascogarcia01@gmail.com>
2025-03-24 11:57:50 +0100

    Funcion ratingGame

--- src/app/games/components/game-card/game-card.component.ts
+++ src/app/games/components/game-card/game-card.component.ts
@@ -7,6 +7,7 @@ import { GamesResponse } from '@games/interfaces/game.interface';
 import { GameImagePipe } from '@games/pipes/game-image.pipe';
 import { environments } from 'src/environments/environments';
 import { GamesService } from '../../../games/services/games.service';
+import { FormsModule } from '@angular/forms';
 
 
 // 'http://localhost:5001'
@@ -14,7 +15,7 @@ const baseUrl = environments.baseUrl
 
 @Component({
   selector: 'game-card',
-  imports: [RouterLink, SlicePipe, GameImagePipe, CommonModule],
+  imports: [RouterLink, SlicePipe, GameImagePipe, CommonModule, FormsModule],
   templateUrl: './game-card.component.html',
 
 })
@@ -24,6 +25,7 @@ export class GameCardComponent {
   constructor(private http: HttpClient) {}
 
 game = input.required<GamesResponse>()
+ratingValue: number = 0;
 
 toggleFavorite(uuid: string) {
   this.http.patch(`${baseUrl}/games/${uuid}/favorite`, { favorite: !this.game().favorite })
@@ -33,6 +35,30 @@ toggleFavorite(uuid: string) {
       console.log('Respuesta backend:',response)
     });
 }
+
+/* ratingGame(uuid: string){
+  this.http.patch(`${baseUrl}/games/${uuid}/rating`, { rating: 0 })
+  .subscribe(response => {
+    this.game().rating = 0;  // Cambiar el estado local
+    console.log('Favorito actualizado:', this.game());
+    console.log('Respuesta backend:',response)
+  });
+} */
+
+  ratingGame(uuid: string, rating:number)  {
+    if (isNaN(rating) || rating < 0 || rating > 10) {
+      console.log('Introduce un número válido entre 0 y 10');
+      return;
+    }
+
+    this.http.patch(`${baseUrl}/games/${uuid}/rating`, { rating })
+      .subscribe(response => {
+        this.game().rating = rating; // Actualizar el estado local
+        console.log('Rating actualizado:', this.game());
+        console.log('Respuesta backend:', response);
+      });
+  }
+
 
 
 
