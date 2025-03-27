f0ba4b7351a190c11e619c7bd0c32bf4aad93afc
Juan Velasco <juanvelascogarcia01@gmail.com>
2025-03-27 11:44:20 +0100

    Creacion y utilizacion nuevo componente category-select

--- src/app/app-front/components/front-navbar/front-navbar.component.ts
+++ src/app/app-front/components/front-navbar/front-navbar.component.ts
@@ -1,19 +1,21 @@
 import { Component, HostListener } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { CategorySelectComponent } from '@shared/components/category-select/category-select.component';
+
 
 @Component({
   selector: 'front-navbar',
-  imports: [RouterLink],
+  imports: [RouterLink,CategorySelectComponent],
   templateUrl: './front-navbar.component.html',
 })
 export class FrontNavbarComponent {
 
   isLargeScreen: boolean = window.innerWidth >= 1024; // Tamaño de pantalla para lg
 
-  /* @HostListener('window:resize', ['$event'])
+  @HostListener('window:resize', ['$event'])
   onResize(event: Event) {
     this.isLargeScreen = window.innerWidth >= 1024; // Ajusta el valor según el breakpoint lg que uses
-  } */
+  }
 
   scrollToTop(): void {
     window.scrollTo({ top: 0, behavior: 'smooth' });
---
+++ src/app/shared/components/category-select/category-select.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'category-select',
+  imports: [],
+  templateUrl: './category-select.component.html',
+})
+export class CategorySelectComponent { }
