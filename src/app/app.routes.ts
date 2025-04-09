fabac598cf1ed96a0b1dd004689bddb3c473bbd6
Juan Velasco <juanvelascogarcia01@gmail.com>
2025-04-09 11:17:12 +0200

    modulo de auth, paginas login y register

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,12 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
 
   {
+path: 'auth',
+loadChildren: () => import('./auth/auth.routes')
+  },
+
+
+  {
     path:'',
     loadChildren:()=> import('./app-front/app-front.routes')
   }
